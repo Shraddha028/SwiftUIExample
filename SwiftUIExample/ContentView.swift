63c8d98b6e0c2b54ee50c18b8f6911d7866ddf59
shraddha.sutar@accenture.com <shraddha.sutar@accenture.com>
2023-01-31 15:17:26 +0530

    Initial Commit

---
+++ SwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by shraddha.sutar@accenture.com on 31/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Test")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by shraddha.sutar@accenture.com on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
